26ed0c4910c4fccada4499ed7fd783535b511c25
CoderPro67 <163164359+CoderPro67@users.noreply.github.com>
2024-06-17 14:40:37 +0530

    Create followes.py

---
+++ followes.py
@@ -0,0 +1,6 @@
+# Retrieve the usernames of all followers
+followers = [follower.username for follower in profile.get_followers()]
+
+# Retrieve the usernames of all followees
+followees = [followee.username for followee in profile.get_followees()]
+print(followers)
