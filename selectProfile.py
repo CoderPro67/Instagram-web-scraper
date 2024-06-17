52bb534b810f624b82baf4ad485c604a7e900b44
CoderPro67 <163164359+CoderPro67@users.noreply.github.com>
2024-06-17 14:37:44 +0530

    Create selectProfile.py

---
+++ selectProfile.py
@@ -0,0 +1,11 @@
+# Import the module
+!pip install instaloader
+import instaloader
+
+# Create an instance of Instaloader class
+bot = instaloader.Instaloader()
+
+# Load a profile from an Instagram handle
+profile = instaloader.Profile.from_username(bot.context, 'aman.kharwal')
+
+print(type(profile))
