adf7595e031cd577ff6e127f25501670377bee96
CoderPro67 <163164359+CoderPro67@users.noreply.github.com>
2024-06-17 14:40:08 +0530

    Create login.py

---
+++ login.py
@@ -0,0 +1,5 @@
+# Login with username and password in the script
+bot.login(user="your username",passwd="your password")
+
+# Interactive login on terminal
+bot.interactive_login("your username") # Asks for password in the terminal
