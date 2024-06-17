9e5d01ad97222bba0ee6c5a838fb55fa25001664
CoderPro67 <163164359+CoderPro67@users.noreply.github.com>
2024-06-17 14:39:37 +0530

    Create dataprofile.py

---
+++ dataprofile.py
@@ -0,0 +1,6 @@
+print("Username: ", profile.username)
+print("User ID: ", profile.userid)
+print("Number of Posts: ", profile.mediacount)
+print("Followers: ", profile.followers)
+print("Followees: ", profile.followees)
+print("Bio: ", profile.biography,profile.external_url)
